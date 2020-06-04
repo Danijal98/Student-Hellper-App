d7662422af7cc1e19a7ebe62a360f24b42d00b0d
Danijal Azerović <danijalazerovic@gmail.com>
2020-06-04 13:25:05 +0200

    DB fix, layout behaviour

--- app/src/main/java/rs/raf/projekat2/danijal_azerovic_RN8618_dusan_jerinic_RN8718/data/models/Raspored.kt
+++ app/src/main/java/rs/raf/projekat2/danijal_azerovic_RN8618_dusan_jerinic_RN8718/data/models/Raspored.kt
@@ -1,6 +1,7 @@
 package rs.raf.projekat2.danijal_azerovic_RN8618_dusan_jerinic_RN8718.data.models
 
 data class Raspored (
+    val id: String,
     val predmet: String,
     val tip: String,
     val nastavnik: String,
--- app/src/main/java/rs/raf/projekat2/danijal_azerovic_RN8618_dusan_jerinic_RN8718/data/models/RasporedEntity.kt
+++ app/src/main/java/rs/raf/projekat2/danijal_azerovic_RN8618_dusan_jerinic_RN8718/data/models/RasporedEntity.kt
@@ -5,7 +5,7 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "raspored")
 data class RasporedEntity(
-    @PrimaryKey(autoGenerate = true) val id: String,
+    @PrimaryKey(autoGenerate = true) val id: Integer,
     val predmet: String,
     val tip: String,
     val nastavnik: String,
