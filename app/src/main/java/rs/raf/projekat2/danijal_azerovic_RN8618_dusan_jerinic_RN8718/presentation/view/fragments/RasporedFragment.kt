7c4fde0baf07e40e34d36bc4956c51e101b4e0b6
Danijal Azerović <danijalazerovic@gmail.com>
2020-06-05 00:08:23 +0200

    Data fetched, cashed and displayed from raf.json

--- app/src/main/java/rs/raf/projekat2/danijal_azerovic_RN8618_dusan_jerinic_RN8718/presentation/view/fragments/RasporedFragment.kt
+++ app/src/main/java/rs/raf/projekat2/danijal_azerovic_RN8618_dusan_jerinic_RN8718/presentation/view/fragments/RasporedFragment.kt
@@ -3,20 +3,19 @@ package rs.raf.projekat2.danijal_azerovic_RN8618_dusan_jerinic_RN8718.presentati
 import android.os.Bundle
 import android.view.View
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.fragment_raspored.*
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
-import org.koin.androidx.viewmodel.ext.android.viewModel
 import rs.raf.projekat2.danijal_azerovic_RN8618_dusan_jerinic_RN8718.R
 import rs.raf.projekat2.danijal_azerovic_RN8618_dusan_jerinic_RN8718.presentation.contract.RasporedContract
 import rs.raf.projekat2.danijal_azerovic_RN8618_dusan_jerinic_RN8718.presentation.viewmodel.RasporedViewModel
 import timber.log.Timber
-import rs.raf.projekat2.danijal_azerovic_RN8618_dusan_jerinic_RN8718.presentation.contract.RasporedContract
 import rs.raf.projekat2.danijal_azerovic_RN8618_dusan_jerinic_RN8718.presentation.view.recycler.adapter.RasporedAdapter
 import rs.raf.projekat2.danijal_azerovic_RN8618_dusan_jerinic_RN8718.presentation.view.recycler.diff.RasporedDiffItemCallback
-import rs.raf.projekat2.danijal_azerovic_RN8618_dusan_jerinic_RN8718.presentation.viewmodel.RasporedViewModel
+import rs.raf.projekat2.danijal_azerovic_RN8618_dusan_jerinic_RN8718.presentation.view.states.RasporedState
 
 class RasporedFragment: Fragment(R.layout.fragment_raspored) {
 
@@ -54,6 +53,7 @@ class RasporedFragment: Fragment(R.layout.fragment_raspored) {
     private fun initObservers(){
         rasporedViewModel.rasporedState.observe(viewLifecycleOwner,Observer{
             Timber.e(it.toString())
+            renderState(it)
         })
         rasporedViewModel.getRaspored()
         rasporedViewModel.fetchRaspored()
@@ -63,6 +63,23 @@ class RasporedFragment: Fragment(R.layout.fragment_raspored) {
         recycler_raspored.layoutManager = LinearLayoutManager(activity)
         rasporedAdapter = RasporedAdapter(RasporedDiffItemCallback())
         recycler_raspored.adapter = rasporedAdapter
+    }
+
+    private fun renderState(state: RasporedState){
+        when(state){
+            is RasporedState.Success -> {
+                rasporedAdapter.submitList(state.raspored)
+            }
+            is RasporedState.Error -> {
+                Toast.makeText(context, state.message, Toast.LENGTH_SHORT).show()
+            }
+            is RasporedState.DataFetched -> {
+                Toast.makeText(context, "Fresh data fetched from the server", Toast.LENGTH_LONG).show()
+            }
+            is RasporedState.Loading -> {
+                Toast.makeText(context, "Loading...", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
 }
