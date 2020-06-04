31e395646c908e8f1e76609bc57bdc1b5b029c02
Jerinic <dukijerinic@gmail.com>
2020-06-04 18:55:51 +0200

    A lot of random things

---
+++ app/src/main/java/rs/raf/projekat2/danijal_azerovic_RN8618_dusan_jerinic_RN8718/presentation/view/activities/EditNotesActivity.kt
@@ -0,0 +1,8 @@
+package rs.raf.projekat2.danijal_azerovic_RN8618_dusan_jerinic_RN8718.presentation.view.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import rs.raf.projekat2.danijal_azerovic_RN8618_dusan_jerinic_RN8718.R
+
+class EditNotesActivity : AppCompatActivity(R.layout.activity_edit_notes)  {
+
+}
--- app/src/main/java/rs/raf/projekat2/danijal_azerovic_RN8618_dusan_jerinic_RN8718/presentation/view/fragments/StatistikaFragment.kt
+++ app/src/main/java/rs/raf/projekat2/danijal_azerovic_RN8618_dusan_jerinic_RN8718/presentation/view/fragments/StatistikaFragment.kt
@@ -1,8 +1,47 @@
 package rs.raf.projekat2.danijal_azerovic_RN8618_dusan_jerinic_RN8718.presentation.view.fragments
 
+import android.os.Build
+import android.os.Bundle
+import android.view.View
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet
+import kotlinx.android.synthetic.main.fragment_statistika.*
 import rs.raf.projekat2.danijal_azerovic_RN8618_dusan_jerinic_RN8718.R
+import java.time.LocalDate
+import java.time.LocalDate.now
+import java.time.LocalTime.now
+import java.util.*
+import kotlin.collections.ArrayList
 
 class StatistikaFragment: Fragment(R.layout.fragment_statistika) {
 
+    lateinit var chart: BarChart
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+    }
+
+    private fun init(){
+        initGraph()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun initGraph(){
+        chart = bar_chart
+        val dataBarEntries = ArrayList<BarEntry>()
+        val values = floatArrayOf(5.0F, 2.0F, 3.0F, 6.0F, 4.0F)
+        values.forEachIndexed{index, element ->
+            dataBarEntries.add(BarEntry(element, index.toFloat()))
+        }
+        val dataSet = BarDataSet(dataBarEntries, "Data")
+//        dataSet.color = R.color.secondaryColor  ....Aha oce, samo malo sutra, stavis na crvenu, stavi je na plavo...
+        val data = BarData(dataSet)
+        bar_chart.data = data
+    }
 }
