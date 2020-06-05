6c4b1c2b3e30d0bc0cab66d25cc90172e57758ec
Danijal Azerović <danijalazerovic@gmail.com>
2020-06-05 16:06:07 +0200

    Adding beleska init

--- app/src/main/java/rs/raf/projekat2/danijal_azerovic_RN8618_dusan_jerinic_RN8718/presentation/view/activities/AddNotesActivity.kt
+++ app/src/main/java/rs/raf/projekat2/danijal_azerovic_RN8618_dusan_jerinic_RN8718/presentation/view/activities/AddNotesActivity.kt
@@ -1,9 +1,14 @@
 package rs.raf.projekat2.danijal_azerovic_RN8618_dusan_jerinic_RN8718.presentation.view.activities
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_add_notes.*
 import rs.raf.projekat2.danijal_azerovic_RN8618_dusan_jerinic_RN8718.R
+import rs.raf.projekat2.danijal_azerovic_RN8618_dusan_jerinic_RN8718.presentation.view.fragments.BeleskeFragment
+import timber.log.Timber
 
 class AddNotesActivity : AppCompatActivity(R.layout.activity_add_notes)   {
 
@@ -18,9 +23,19 @@ class AddNotesActivity : AppCompatActivity(R.layout.activity_add_notes)   {
 
     private fun initListeners(){
         nova_beleska_button_sacuvaj.setOnClickListener {
-            val naslov = nova_beleska_naslov.text
-            val text = nova_beleska_text.text
-            //TODO
+            val title = nova_beleska_naslov.text.toString()
+            val text = nova_beleska_text.text.toString()
+            if(title.isBlank() || text.isBlank()){
+                Toast.makeText(this, "One of the fields is empty!", Toast.LENGTH_SHORT).show()
+                nova_beleska_naslov.requestFocus()
+                return@setOnClickListener
+            }
+            val returnIntent = Intent()
+            Timber.e("$title, $text")
+            returnIntent.putExtra(BeleskeFragment.MESSAGE_KEY_TITLE, title.trim())
+            returnIntent.putExtra(BeleskeFragment.MESSAGE_KEY_TEXT, text.trim())
+            setResult(Activity.RESULT_OK, returnIntent)
+            finish()
         }
         nova_beleska_button_odustani.setOnClickListener {
             //TODO
--- app/src/main/java/rs/raf/projekat2/danijal_azerovic_RN8618_dusan_jerinic_RN8718/presentation/view/fragments/BeleskeFragment.kt
+++ app/src/main/java/rs/raf/projekat2/danijal_azerovic_RN8618_dusan_jerinic_RN8718/presentation/view/fragments/BeleskeFragment.kt
@@ -1,5 +1,7 @@
 package rs.raf.projekat2.danijal_azerovic_RN8618_dusan_jerinic_RN8718.presentation.view.fragments
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
@@ -7,16 +9,25 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.fragment_beleske.*
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 import rs.raf.projekat2.danijal_azerovic_RN8618_dusan_jerinic_RN8718.R
+import rs.raf.projekat2.danijal_azerovic_RN8618_dusan_jerinic_RN8718.data.models.Beleska
 import rs.raf.projekat2.danijal_azerovic_RN8618_dusan_jerinic_RN8718.presentation.contract.BeleskeContract
+import rs.raf.projekat2.danijal_azerovic_RN8618_dusan_jerinic_RN8718.presentation.view.activities.AddNotesActivity
 import rs.raf.projekat2.danijal_azerovic_RN8618_dusan_jerinic_RN8718.presentation.view.recycler.adapter.BeleskeAdapter
 import rs.raf.projekat2.danijal_azerovic_RN8618_dusan_jerinic_RN8718.presentation.view.recycler.diff.BeleskaDiffItemCallback
 import rs.raf.projekat2.danijal_azerovic_RN8618_dusan_jerinic_RN8718.presentation.view.states.BeleskeState
 import rs.raf.projekat2.danijal_azerovic_RN8618_dusan_jerinic_RN8718.presentation.viewmodel.BeleskeViewModel
+import timber.log.Timber
 
 class BeleskeFragment: Fragment(R.layout.fragment_beleske) {
 
     private val beleskeViewModel: BeleskeContract.ViewModel by sharedViewModel<BeleskeViewModel>()
     private lateinit var beleskeAdapter: BeleskeAdapter
+
+    companion object{
+        const val MESSAGE_REQUEST_CODE = 1
+        const val MESSAGE_KEY_TITLE = "title"
+        const val MESSAGE_KEY_TEXT = "text"
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -48,7 +59,28 @@ class BeleskeFragment: Fragment(R.layout.fragment_beleske) {
     }
 
     private fun initListeners() {
-        //TODO
+        btn_add.setOnClickListener {
+            val intent = Intent(context, AddNotesActivity::class.java)
+            startActivityForResult(intent, MESSAGE_REQUEST_CODE)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(resultCode != Activity.RESULT_OK){
+            return
+        }
+
+        if (requestCode == MESSAGE_REQUEST_CODE){
+            data?.let {
+                val title = data.getStringExtra(MESSAGE_KEY_TITLE)
+                val text = data.getStringExtra(MESSAGE_KEY_TEXT)
+                val beleska = Beleska("0", title, text)
+                Timber.e("$beleska")
+            }
+        }
+
     }
 
     private fun renderState(state: BeleskeState){
